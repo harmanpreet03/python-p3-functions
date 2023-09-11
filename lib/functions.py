d7510a5cf7c3a8bb2fa62606ca5c80e2f03b45df
harmanpreet ghotra <harmanpreetghotra17@gmail.com>
2023-09-11 13:54:42 -0400

    lab done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python3
+# Create a Python file named 'lib/functions.py'
 
+# Define a method greet_programmer() that takes no arguments.
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
+# Define a method greet() that takes one argument, a name.
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
+# Define a method greet_with_default() that takes one argument, a name.
+# It should output the string "Hello, name!" (with "name" being whatever value was passed as an argument) to the terminal with print().
+# If no arguments are passed in, it should output the string "Hello, programmer!".
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+# Define a method add() that takes two numbers as arguments and returns the sum of those two numbers.
+def add(a, b):
+    return a + b
 
+# Define a method halve() that takes one number as an argument and returns the that number's value, divided by two.
 def halve(number):
-    pass
+    return number / 2
